ee16236562599ef315bc0b870ad395d5852d11af
Stanislav Gavrilov <stas.a.gavrilov@gmail.com>
2015-04-24 20:30:45 +0300

    Add popup

--- src/js/script.js
+++ src/js/script.js
@@ -11,14 +11,23 @@ $( document ).ready(function()
 			//popup options
         	$(document).on('click', '.js-message_link', function(e)
         	{	 			 		
-	 			e.preventDefault();
-	 			 $(".js-message_link").parent().append("<p class='js-popup'>Положение сложное Соня</p>");
-			}); 
+	 			     e.preventDefault();
+            if($(".js-message_link").next().hasClass('js-popup'))
+            {
+              $(".js-popup").remove();
+            }
+            else
+             {
+                $(".js-message_link").parent().append("<p class='js-popup'>Положение сложное Соня</p>");
+       
+             }
+	 		    });	  
+
 
         	$(document).on('click', '.js-popup', function()
         	{	 			 			 			
-	 			 $(".js-popup").remove();
-			}); 
+	 		    	 $(".js-popup").remove();
+		    	}); 
 
         });
     });
