992d43638695ac71bc28f947a650db13369d86be
Stanislav Gavrilov <stas.a.gavrilov@gmail.com>
2015-04-25 15:50:12 +0300

    Аdd tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,32 @@
 $( document ).ready(function() 
-    {  //slider options
-       $(function(){
-          $("#slides").slides({
-            
-            play: 3000,
-            pause: 2500,
-            hoverPause: true    
-          });
+{  //slider options
+   $(function()
+   {
+      $("#slides").slides({
         
-			//popup options
-        	$(document).on('click', '.js-message_link', function(e)
-        	{	 			 		
-	 			     e.preventDefault();
-            if($(".js-message_link").next().hasClass('js-popup'))
-            {
-              $(".js-popup").remove();
-            }
-            else
-             {
-                $(".js-message_link").parent().append("<p class='js-popup'>Положение сложное Соня</p>");
-       
-             }
-	 		    });	  
+        play: 3000,
+        pause: 2500,
+        hoverPause: true    
+      });
+    
+    
+	    //popup options
+    	$(document).on('click', '.js-message_link', function(e)
+    	{	 			 		
+			    e.preventDefault();
+        if($(".js-message_link").next().hasClass('js-popup'))
+        {
+          $(".js-popup").remove();
+        }
+        else
+        {
+            $(".js-message_link").parent().append("<p class='js-popup'>Положение сложное Соня</p>");       
+        }
+		  });	  
+    	$(document).on('click', '.js-popup', function()
+    	{	 			 			 			
+		    	 $(".js-popup").remove();
+    	}); 
 
-
-        	$(document).on('click', '.js-popup', function()
-        	{	 			 			 			
-	 		    	 $(".js-popup").remove();
-		    	}); 
-
-        });
     });
+ });
